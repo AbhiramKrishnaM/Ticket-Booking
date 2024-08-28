c19698c8f7adbf6dc7d3b7a4a1f8835f1afa688f
Abhiram Krishna M <abhiramKrishna.8921@gmail.com>
2024-08-28 07:48:56 +0530

    added authentication handlers and repositories

--- be/cmd/api/main.go
+++ be/cmd/api/main.go
@@ -6,7 +6,9 @@ import (
 	"github.com/abhiramkrishnam/ticket-management-be/config"
 	"github.com/abhiramkrishnam/ticket-management-be/db"
 	"github.com/abhiramkrishnam/ticket-management-be/handlers"
+	middlewares "github.com/abhiramkrishnam/ticket-management-be/middleware"
 	"github.com/abhiramkrishnam/ticket-management-be/repositories"
+	"github.com/abhiramkrishnam/ticket-management-be/services"
 	"github.com/gofiber/fiber/v2"
 )
 
@@ -23,11 +25,19 @@ func main() {
 
 	eventRepository := repositories.NewEventRepository(db)
 	ticketRepository := repositories.NewTicketRepository(db)
+	authRepository := repositories.NewAuthRepository(db)
+
+	// Service
+	authService := services.NewAuthService(authRepository)
 
 	server := app.Group("/api")
 
-	handlers.NewEventHandler(server.Group("/event"), eventRepository)
-	handlers.NewTicketHandler(server.Group("/ticket"), ticketRepository)
+	handlers.NewAuthHandler(server.Group("/auth"), authService)
+
+	privateRoutes := server.Use(middlewares.AuthProtected(db))
+
+	handlers.NewEventHandler(privateRoutes.Group("/event"), eventRepository)
+	handlers.NewTicketHandler(privateRoutes.Group("/ticket"), ticketRepository)
 
 	app.Listen(fmt.Sprintf(":" + envConfig.ServerPort))
 

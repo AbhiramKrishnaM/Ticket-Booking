eeb4a84643e9c4b213f0ac9226f19bca3c4fc66e
Abhiram Krishna M <abhiramKrishna.8921@gmail.com>
2024-09-20 19:57:52 +0530

    added ui for tickets

--- ticket-booking-fe/app/(authed)/(tabs)/(events)/index.tsx
+++ ticket-booking-fe/app/(authed)/(tabs)/(events)/index.tsx
@@ -1,14 +1,27 @@
+import Button from "@/components/Button";
+import Divider from "@/components/Divider";
 import HStack from "@/components/HStack";
+import { TabBarIcon } from "@/components/navigation/TabBarIcon";
 import Text from "@/components/Text";
 import VStack from "@/components/VStack";
+import { useAuth } from "@/context/AuthContext";
 import { eventService } from "@/services/event";
 import { Event } from "@/types/event";
+import { UserRole } from "@/types/user";
+import { router } from "expo-router";
 import React, { useCallback, useEffect, useState } from "react";
-import { Alert, FlatList } from "react-native";
+import { Alert, FlatList, TouchableOpacity } from "react-native";
 
 const EventScreen = () => {
+  const { user } = useAuth();
   const [isLoading, setIsLoading] = useState(false);
   const [events, setEvents] = useState<Event[]>([]);
+
+  function onGoToEventPage(id) {
+    if (user?.role === UserRole.Manager) {
+      router.push(`/(events)/event/${id}`);
+    }
+  }
 
   const fetchEvents = useCallback(async () => {
     try {
@@ -47,19 +60,47 @@ const EventScreen = () => {
           style={{ backgroundColor: "white", borderRadius: 20 }}
           key={event.id}
         >
-          <HStack alignItems="center" justifyContent="center">
-            <HStack alignItems="center">
-              <Text fontSize={16} bold>
-                {event.name}
-              </Text>
-              <Text fontSize={16} bold>
-                |
-              </Text>
-              <Text fontSize={16} bold>
-                {event.location}
-              </Text>
+          <TouchableOpacity onPress={() => onGoToEventPage(event.id)}>
+            <HStack alignItems="center" justifyContent="space-between">
+              <HStack alignItems="center">
+                <Text fontSize={16} bold>
+                  {event.name}
+                </Text>
+                <Text fontSize={16} bold>
+                  |
+                </Text>
+                <Text fontSize={16} bold>
+                  {event.location}
+                </Text>
+              </HStack>
+              {user?.role === UserRole.Manager && (
+                <TabBarIcon size={24} name="chevron-forward" />
+              )}
             </HStack>
+          </TouchableOpacity>
+
+          <Divider />
+
+          <HStack justifyContent="space-between">
+            <Text bold fontSize={16} color="gray">
+              Sold: {event.totalTicketsPurchased}
+            </Text>
+            <Text bold fontSize={16} color="green">
+              Entered : {event.totalTicketsEntered}
+            </Text>
           </HStack>
+
+          {user?.role === UserRole.Attendee && (
+            <VStack>
+              <Button
+                variant="outlined"
+                disabled={isLoading}
+                onPress={() => {}}
+              >
+                Buy Ticket
+              </Button>
+            </VStack>
+          )}
         </VStack>
       )}
     />

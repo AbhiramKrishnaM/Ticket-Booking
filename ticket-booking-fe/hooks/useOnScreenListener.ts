957887929e694838e43dd0a96ee7dc8cd3427de0
Abhiram Krishna M <abhiramKrishna.8921@gmail.com>
2024-09-20 23:41:24 +0530

    added ticket listing and adding page added

---
+++ ticket-booking-fe/hooks/useOnScreenListener.ts
@@ -0,0 +1,14 @@
+import { useNavigation } from "expo-router";
+import { useEffect } from "react";
+
+export function useOnScreenListener(
+  eventType: "focus" | "blur" | "state" | "beforeRemove",
+  callback: VoidFunction
+) {
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    const subscribe = navigation.addListener(eventType, callback);
+    return subscribe;
+  }, [navigation, callback, eventType]);
+}
